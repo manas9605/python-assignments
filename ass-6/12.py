f15cb8d9a818034e4e3673599994dfbde4dc9d72
manas9605 <manasranjanpatnaik303@gmail.com>
2022-09-01 19:18:23 +0530

    comment deleted

--- ass-6/12.py
+++ ass-6/12.py
@@ -1,6 +1,6 @@
 #Write a python script to accept one complex number from the user and display the
 #greater number between real part and imaginary part
-import xmlrpc.server
+
 
 x = complex(input("enter a no "))
 print (x.real) if x.real>x.imag else print(x.imag)

502d5f432054e4493137a114c948035ae68bb46b
manas9605 <manasranjanpatnaik303@gmail.com>
2023-01-12 18:25:46 +0530

    2 qs done

--- ass-11/10.py
+++ ass-11/10.py
@@ -1,2 +1,4 @@
 '''Write a python script to print the octal equivalent of a given decimal number. (do not
 use oct() method)'''
+a=int(input("enter a decimal no : "))
+print(f"octal eqv. of {a} is ",oct(a))
--- ass-11/9.py
+++ ass-11/9.py
@@ -1,3 +1,4 @@
 '''Write a python script to print binary equivalent of a given decimal number. (do not
 use bin() method)'''
-a=
+a=int(input("enter a decimal no : "))
+print(f"binary  eqv. of {a} is ",bin(a))

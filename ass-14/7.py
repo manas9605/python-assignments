a0c9861a2e1582b42547a461c5ce40eb21c333ad
manas9605 <manasranjanpatnaik303@gmail.com>
2023-01-12 18:54:23 +0530

    done

--- ass-14/7.py
+++ ass-14/7.py
@@ -1,5 +1,5 @@
 '''Write a Python script to remove all non int values from a list.'''
 a=print("enter elements in the list : ")
 l1=[str(e) for e in input().split(',')]
-l2=[e for e in l1 if type(e) == int]
-print("final list elements after removing non int values",l2)
+l1=[e for e in l1 if e.isdigit()]
+print("final list elements after removing non int values",l1)
--- ass-14/9.py
+++ ass-14/9.py
@@ -1,2 +1,8 @@
 '''Write a Python script to print indices of all occurrences of a given element in a given
 list.'''
+my_list = [1, 2, 3, 1, 5, 4]
+item = 1
+
+indices = [i for i in range(len(my_list)) if my_list[i] == item]
+
+print(indices)
